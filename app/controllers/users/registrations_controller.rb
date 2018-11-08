4245abbb1b81678885db60147ef0e3d3833cf0b7
Dani <dustyglass@gmail.com>
2018-11-07 21:37:47 -0300

    Fix: dashboard

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create, :update]
+  before_action :configure_account_update_params, only: [:update]
+  prepend_before_action :require_no_authentication, only: [:update]
   skip_authorize_resource
   skip_authorization_check
-  # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -10,11 +11,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-   def create
-  @user = User.find(params[:id])
-  @user.update role: params[:role]
-  @user.save
-   end
+  def create
+    @user = User.find(params[:id])
+    @user.update role: params[:role]
+    @user.save
+    redirect_to root_path
+  end
 
   # GET /resource/edit
   def edit
@@ -22,10 +24,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
    end
 
   # PUT /resource
-   def update
+def update
   @user = User.find(params[:id])
   @user.update role: params[:role]
   @user.save
+  redirect_to root_path
 end
 
 
@@ -57,9 +60,9 @@ end
    end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+   def configure_account_update_params
+     devise_parameter_sanitizer.permit(:account_update, keys: [:id, :role])
+   end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
